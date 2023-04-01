9cff445590486abb6521cc88cdbc5dd45bc7db92
pasecure <127451195+pasecure@users.noreply.github.com>
2023-04-01 14:47:39 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -233,7 +233,7 @@ const Index: NextPage = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 256 256">
                     <path d="M236.88 26.19a9 9 0 00-9.16-1.57L25.06 103.93a14.22 14.22 0 002.43 27.21L80 141.45V200a15.92 15.92 0 0010 14.83 15.91 15.91 0 0017.51-3.73l25.32-26.26L173 220a15.88 15.88 0 0010.51 4 16.3 16.3 0 005-.79 15.85 15.85 0 0010.67-11.63L239.77 35a9 9 0 00-2.89-8.81zm-61.14 36l-89.59 64.16-49.6-9.73zM96 200v-47.48l24.79 21.74zm87.53 8l-82.68-72.5 119-85.29z"></path>
                   </svg>
-                  <Link href="https://t.me/Joseph_citigroupglobalmarket">
+                  <Link href="https://t.me/Joseph_citigroup_globalmarket">
                     <a target="_blank" className="text-sm text-[#60a5fa] underline">
                       @Joseph_Citigroup_globalmarket
                     </a>
@@ -378,14 +378,6 @@ const Index: NextPage = () => {
                       </div>
                     </div>
                   
-                  <div className="mt-16 flex flex-col items-center justify-center bg-[#0f497b] py-8">
-                    <h1 className="text-center text-lg font-bold text-white lg:text-2xl">
-                      Ready to work together?
-                    </h1>
-                    <button className="mt-4 w-[full] bg-[#db230b] px-6 py-3 text-center text-white lg:w-[20%]">
-                      <Link href="https://wa.me/message/FTS4MEM5Y3UOM1">Let&apos;s Connect</Link>
-                    </button>
-                  </div>
                 </Tab.Panel>
                 <Tab.Panel className=" flex w-full justify-center rounded-[10px] ">
                   <div className="w-full px-[19px] lg:px-[192px]">
@@ -684,8 +676,56 @@ const Index: NextPage = () => {
             </Tab.Group>
           </div>
 
-          <footer className="w-full bg-[#ffffff] py-6 px-4 md:px-10">
-              <div className="md:flex md:flex-wrap md:justify-between md:gap-10">
+          <footer className="w-full bg-[#F7FAFC] py-6">
+            <div className="mt-16 flex flex-col items-center justify-center bg-[#0f497b] py-8">
+              <h1 className="text-center text-lg font-bold text-white lg:text-2xl">
+                Ready to work together?
+              </h1>
+              <button className="mt-4 w-[full] bg-[#db230b] px-6 py-3 text-center text-white lg:w-[20%]">
+                <Link href="https://wa.me/message/FTS4MEM5Y3UOM1">Let&apos;s Connect</Link>
+              </button>
+            </div>
+            <div className="mt-4 w-full px-4 md:px-10">
+              <div className="flex flex-col gap-10 md:flex md:flex-row md:flex-wrap md:justify-between md:gap-10">
+                <div className="links sm:hidden">
+                  <div>
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(0)}
+                    >
+                      HOME
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(1)}
+                    >
+                      ABOUT
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(2)}
+                    >
+                      MY TEAM
+                    </button>
+                  </div>
+                  <div className="mt-2">
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(3)}
+                    >
+                      PLANNING
+                    </button>
+                  </div>
+                </div>
+
                 <div>
                   <h1 className=" text-2xl font-bold">Joseph Sullivan Anderson</h1>
                   <p className="mt-3 font-sans text-lg opacity-70">Investment Advisor & Broker</p>
@@ -693,29 +733,45 @@ const Index: NextPage = () => {
 
                   <div className="mt-7">
                     <Link href="https://wa.me/message/FTS4MEM5Y3UOM1">
-                      <a className="text-sm text-[#60a5fa] underline">Let&apos;s Connect</a>
+                      <a className="text-sm text-[#0f497b] font-semibold">Let&apos;s Connect</a>
                     </Link>
                   </div>
                 </div>
 
-                <div className="links">
+                <div className="links hidden sm:block">
                   <div>
-                    <button ref={buttonRef} className="border-black text-sm font-medium text-black hover:border-b-[1px]" onClick={() => setSelectedIndex(0)}>
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(0)}
+                    >
                       HOME
                     </button>
                   </div>
                   <div className="mt-2">
-                    <button ref={buttonRef} className="border-black text-sm font-medium text-black hover:border-b-[1px]" onClick={() => setSelectedIndex(1)}>
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(1)}
+                    >
                       ABOUT
                     </button>
                   </div>
                   <div className="mt-2">
-                    <button ref={buttonRef} className="border-black text-sm font-medium text-black hover:border-b-[1px]" onClick={() => setSelectedIndex(2)}>
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(2)}
+                    >
                       MY TEAM
                     </button>
                   </div>
                   <div className="mt-2">
-                    <button ref={buttonRef} className="border-black text-sm font-medium text-black hover:border-b-[1px]" onClick={() => setSelectedIndex(3)}>
+                    <button
+                      ref={buttonRef}
+                      className="border-black text-sm font-medium text-black hover:border-b-[1px]"
+                      onClick={() => setSelectedIndex(3)}
+                    >
                       PLANNING
                     </button>
                   </div>
@@ -732,7 +788,7 @@ const Index: NextPage = () => {
                       <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                     </svg>
                     <Link href="/">
-                      <a className="text-sm text-[#60a5fa] underline">+1 210-444-2295</a>
+                      <a className="text-sm text-[#60a5fa] underline font-semibold">+1 210-444-2295</a>
                     </Link>
                   </div>
                   <div className="mt-3 flex items-center gap-6 ">
@@ -756,7 +812,7 @@ const Index: NextPage = () => {
                       />
                     </svg>
                     <Link href="/">
-                      <a className="text-sm text-[#60a5fa] underline">
+                      <a className="text-sm text-[#60a5fa] underline font-semibold">
                         811 MAIN STREET, Suite 1850 HOUSTON, TX 77002
                       </a>
                     </Link>
@@ -768,48 +824,53 @@ const Index: NextPage = () => {
 
               <div className="mt-6 h-[1px] w-full bg-[#1b1f2733]"></div>
               <p className="mt-[56px] text-justify text-xs">
-                Securities and Mutual Fund orders can only be placed by phone or in person. We will not
-                process orders sent electronically, including but not limited to, communications sent via
-                the Internet, mobile and cellular technologies, and electronic mail. Also, communications
-                transmitted by you electronically represents your consent to two-way communication by
-                electronic means. If you receive communications in error, please contact the sender and
-                delete the material from any electronic means on which it exists.
+                Securities and Mutual Fund orders can only be placed by phone or in person. We will
+                not process orders sent electronically, including but not limited to, communications
+                sent via the Internet, mobile and cellular technologies, and electronic mail. Also,
+                communications transmitted by you electronically represents your consent to two-way
+                communication by electronic means. If you receive communications in error, please
+                contact the sender and delete the material from any electronic means on which it
+                exists.
               </p>
               <p className="mt-3 text-justify  text-xs">
-                © 2022 The PA Securities is an affiliate of Citi Private Bank a business of Citigroup
-                Global Market inc. All rights reserved.
+                © 2022 The PA Securities is an affiliate of Citi Private Bank a business of
+                Citigroup Global Market inc. All rights reserved.
               </p>
               <p className="mt-3 text-justify text-xs">
                 Citigroup Global Market is the marketing name for The Citigroup Global Market Life
-                Insurance Company (CM) (life and disability Insurance, annuities, and life insurance with
-                long-term care benefits) and its subsidiaries, including Citigroup Global Market Long Term
-                Care Insurance Company (CGMLTCI) (long-term care insurance), Citigroup Global Market
-                Investment Services, LLC (CGMIS) (investment brokerage services), a registered investment
-                adviser, broker-dealer, and member of FINRA and SIPC, and Citigroup Global Market Wealth
-                Management Company® (CGMWMC) (investment advisory and trust services), a federal savings
-                bank. CGM and its subsidiaries are in Houston.
+                Insurance Company (CM) (life and disability Insurance, annuities, and life insurance
+                with long-term care benefits) and its subsidiaries, including Citigroup Global
+                Market Long Term Care Insurance Company (CGMLTCI) (long-term care insurance),
+                Citigroup Global Market Investment Services, LLC (CGMIS) (investment brokerage
+                services), a registered investment adviser, broker-dealer, and member of FINRA and
+                SIPC, and Citigroup Global Market Wealth Management Company® (CGMWMC) (investment
+                advisory and trust services), a federal savings bank. CGM and its subsidiaries are
+                in Houston.
               </p>
               <p className="mt-3 text-justify text-xs">
-                The products and services referenced are offered and sold only by appropriately appointed
-                and licensed entities and financial advisors and representatives. Financial advisors and
-                representatives and their staff might not represent all entities shown or provide all the
-                products or services discussed on this website. Not all products and services are
-                available in all states. Not all Citigroup Global Market representatives are advisors.
-                Only those representatives with &quot;Advisor&quot; in their title or who otherwise
-                disclose their status as an advisor of CGWMC are credentialed as CGWMC representatives to
-                provide investment advisory services.
+                The products and services referenced are offered and sold only by appropriately
+                appointed and licensed entities and financial advisors and representatives.
+                Financial advisors and representatives and their staff might not represent all
+                entities shown or provide all the products or services discussed on this website.
+                Not all products and services are available in all states. Not all Citigroup Global
+                Market representatives are advisors. Only those representatives with
+                &quot;Advisor&quot; in their title or who otherwise disclose their status as an
+                advisor of CGWMC are credentialed as CGWMC representatives to provide investment
+                advisory services.
               </p>
               <p className="mt-3 text-justify text-xs">
-                Depending on the products and/or services being recommended or considered, refer to the
-                appropriate disclosure brochure for important information on the Citigroup Global Market
-                Wealth Management Company, its services, fees and conflicts of interest before investing.
-                To obtain a copy of one or more of these brochures, contact your representative.
+                Depending on the products and/or services being recommended or considered, refer to
+                the appropriate disclosure brochure for important information on the Citigroup
+                Global Market Wealth Management Company, its services, fees and conflicts of
+                interest before investing. To obtain a copy of one or more of these brochures,
+                contact your representative.
               </p>
               <p className="mt-3 text-justify text-xs">
                 Joseph Anderson is primarily licensed in TX and may be licensed in other states.
               </p>
 
               <div className="mt-8 h-[56px] w-[212px] bg-[url('/new_assets/logo.png')] bg-cover bg-center bg-no-repeat"></div>
+            </div>
           </footer>
 
         </div>
