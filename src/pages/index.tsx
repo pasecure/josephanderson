cd90d629b8abe683a7d3c8adc678653834894397
pasecure <127451195+pasecure@users.noreply.github.com>
2023-04-11 13:52:57 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -226,7 +226,7 @@ const Index: NextPage = () => {
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                   </svg>
                   <Link href="/">
-                    <a className="text-sm text-[#60a5fa] underline">+1 210-444-2295</a>
+                    <a className="text-sm text-[#60a5fa] underline">+1 (901) 206-4630</a>
                   </Link>
                 </div>
                 <div className="mt-3 flex items-center gap-6">
@@ -788,7 +788,7 @@ const Index: NextPage = () => {
                       <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                     </svg>
                     <Link href="/">
-                      <a className="text-sm text-[#60a5fa] underline font-semibold">+1 210-444-2295</a>
+                      <a className="text-sm text-[#60a5fa] underline font-semibold">+1 (901) 206-4630</a>
                     </Link>
                   </div>
                   <div className="mt-3 flex items-center gap-6 ">
